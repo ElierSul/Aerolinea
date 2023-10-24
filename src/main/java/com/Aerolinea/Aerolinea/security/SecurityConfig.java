56fea70432f564bd37476b21152000b2c1e86bc5
ElierSul <sulbaraantonio@gmail.com>
2023-10-24 11:53:17 -0500

    Implementacion de autenticacion basica para interactuar con Swagger y OAuth 2.0

---
+++ src/main/java/com/Aerolinea/Aerolinea/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.Aerolinea.Aerolinea.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/vuelos/**", "/usuarios/**", "/tipos/**", "/mediosPago/**",
+                            "/escalas/**", "/boletos/**", "/aerolineas/**").permitAll();
+                    auth.requestMatchers("/javainuse-openapi/**", "/swagger-ui/**").authenticated();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+}
--- src/main/java/com/Aerolinea/Aerolinea/service/VueloService.java
+++ src/main/java/com/Aerolinea/Aerolinea/service/VueloService.java
@@ -117,5 +117,4 @@ public class VueloService {
         vuelo.removeBoleto(boleto);
         return vuelo;
     }
-
 }
