779db687ac5333ec1f1f9da4728938777e891e78
ElierSul <sulbaraantonio@gmail.com>
2023-10-24 15:36:47 -0500

    Implementacion de autenticacion basica para interactuar con Swagger y OAuth 2.0 por medio de GitHub y Google

--- src/main/java/com/Aerolinea/Aerolinea/AerolineaApplication.java
+++ src/main/java/com/Aerolinea/Aerolinea/AerolineaApplication.java
@@ -1,18 +1,15 @@
 package com.Aerolinea.Aerolinea;
 
-import com.Aerolinea.Aerolinea.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AerolineaApplication {
 
-	@Autowired
-	private UsuarioRepository usuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AerolineaApplication.class, args);
+
 	}
 
 	}
